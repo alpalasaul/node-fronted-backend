4f282c882281fdd831197ebfb99a5deafe19e75d
Saúl Alpala <69735886+alpalasaul@users.noreply.github.com>
2023-06-20 01:45:45 +0000

    Create backend and app project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-
-const app = express()
-
-app.use(express.json())
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
+    console.log(`Server running on port ${PORT}`);
+});
 app.get('/ping', (req, res) => {
-    console.log("Hello Stranger :)")
-    res.setHeader("Contet-Type", "applicaton/json")
+    console.log("Hello Stranger :)");
+    res.setHeader("Contet-Type", "applicaton/json");
     res.send("ping");
-})
-
+});
 app.get('/hola/:nombre/:apellido', (req, res) => {
-    const { nombre, apellido } = req.params
-
-    console.log("Solicitando tus nombre")
-
-    res.setHeader("Contet-Type", "applicaton/json")
-    res.send({ nombre, apellido});
-})
+    const { nombre, apellido } = req.params;
+    console.log("Solicitando tus nombre");
+    res.setHeader("Contet-Type", "applicaton/json");
+    res.send({ nombre, apellido });
+});
